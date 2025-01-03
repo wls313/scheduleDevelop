a92fce4351bba70f2089596ba5c1c42d99664ecd
ww313 <wlsghddl3358@naver.com>
2025-01-03 17:14:36 +0900

    feature: Member CREAT,FindByUserId 생성 완료

---
+++ src/main/java/com/shcedule/scheduledevelop/controller/member/MemberController.java
@@ -0,0 +1,32 @@
+package com.shcedule.scheduledevelop.controller.member;
+
+import com.shcedule.scheduledevelop.dto.member.MemberDto;
+import com.shcedule.scheduledevelop.dto.member.MemberRequestDto;
+import com.shcedule.scheduledevelop.dto.member.MemberResponseDto;
+import com.shcedule.scheduledevelop.service.member.MemberService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/members")
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+    @PostMapping
+    public ResponseEntity<MemberDto> save(@Valid @RequestBody MemberRequestDto requestDto) {
+        MemberDto response = memberService.createMember(requestDto);
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{memberId}")
+    public ResponseEntity<MemberResponseDto> findByMemberId(@PathVariable String memberId){
+        MemberResponseDto memberResponseDto = memberService.findByMemberId(memberId);
+
+        return new ResponseEntity<>(memberResponseDto,HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/shcedule/scheduledevelop/service/member/MemberService.java
@@ -0,0 +1,30 @@
+package com.shcedule.scheduledevelop.service.member;
+
+import com.shcedule.scheduledevelop.common.entity.Member;
+import com.shcedule.scheduledevelop.dto.member.MemberDto;
+import com.shcedule.scheduledevelop.dto.member.MemberRequestDto;
+import com.shcedule.scheduledevelop.dto.member.MemberResponseDto;
+import com.shcedule.scheduledevelop.repository.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberDto createMember(MemberRequestDto requestDto) {
+        Member member = memberRepository.save(Member.of(requestDto.memberId(),requestDto.memberName(),requestDto.email(),requestDto.password()));
+
+        return MemberDto.from(member);
+    }
+
+    public MemberResponseDto findByMemberId(String memberId){
+        Member member = memberRepository.findByMemberId(memberId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,memberId+"를 가진 사용자가 존재하지않습니다."));
+
+        return new MemberResponseDto(member.getMemberName(),member.getEmail());
+    }
+}
